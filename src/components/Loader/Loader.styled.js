4510a29db565a119a020c8b386091c67c772fb38
Dmytro Kravchenko <dmitrij.kravchenko.1997@gmail.com>
2023-01-07 19:19:30 +0200

    v. 0.1

--- src/components/Loader/Loader.styled.js
+++ src/components/Loader/Loader.styled.js
@@ -17,7 +17,6 @@ export const MainLoader = styled.div`
 export const LoaderWrapper = styled.div`
   width: 100%;
   height: 100%;
-
   display: flex;
   justify-content: center;
   align-items: center;
